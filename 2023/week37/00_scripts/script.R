da65c15ec0ac838cff5fae7e26e7417b9ce1090c
Ryan Snead <rsnead91@Ryans-MacBook-Air-2.local>
2023-09-10 15:04:50 -0400

    third commit

--- 2023/week37/00_scripts/script.R
+++ 2023/week37/00_scripts/script.R
@@ -7,7 +7,17 @@ pacman::p_load('tidytuesdayR','tidyverse')
 
 # data ----
 
-demographics <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-09-05/demographics.csv')
-wages <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-09-05/wages.csv')
-states <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-09-05/states.csv')
+tuesdata <- tidytuesdayR::tt_load(2023, week = 37) #, auth = github_pat("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"))
 
+
+# demographics <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-09-05/demographics.csv')
+# wages <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-09-05/wages.csv')
+# states <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-09-05/states.csv')
+# 
+# # tuesdata <- tidytuesdayR::tt_load('2023-09-05')
+# ## OR
+# tuesdata <- tidytuesdayR::tt_load(2023, week = 37) #, auth = github_pat("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"))
+# 
+# demographics <- tuesdata$demographics
+# wages <- tuesdata$wages
+# states <- tuesdata$states
